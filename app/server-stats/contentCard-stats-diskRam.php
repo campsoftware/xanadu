cd566ef91c2eab4871e19f1dfc01ad28ba68fd45
Hal Gumbert <hal@campsoftware.com>
2021-04-29 15:33:58 -0400

    2021-04-29-15-31-29 - Added Stats Versions for OS, PHP, and wkHTMLtoPDF. Added section dividers. - Fixed Processes values that were off by one row.

--- app/server-stats/contentCard-stats-diskRam.php
+++ app/server-stats/contentCard-stats-diskRam.php
@@ -1,6 +1,6 @@
 <?php
 // Card
-$cardHeaderContent = \xan\iconFA( 'fas fa-hdd' ) . STR_NBSP . 'Disk and ' . \xan\iconFA( 'fas fa-microchip' ) . STR_NBSP . 'RAM';
+$cardHeaderContent = \xan\iconFA( 'fas fa-hdd' ) . STR_NBSP . 'Versions, Disk, and RAM';
 $card = new \xan\eleCard( CARD_WIDTH, CARD_HEIGHT_MAX, false );
 
 // Tags Special
@@ -10,6 +10,17 @@ $table = new \xan\eleTable( $tagsCellEmpty );
 $tableRowIndex = -1;
 
 // Table Rows
+$table->cellSet( ++$tableRowIndex, 0, $tagsCellRightMiddle, 'OS' );
+$table->cellSet( $tableRowIndex, 1, $tagsCellRightMiddle, \xan\strSubstitute( shell_exec( 'awk -F= \'$1=="PRETTY_NAME" { print $2 ;}\' /etc/os-release' ), '"', '' ) );
+
+$table->cellSet( ++$tableRowIndex, 0, $tagsCellRightMiddle, 'PHP' );
+$table->cellSet( $tableRowIndex, 1, $tagsCellRightMiddle, phpversion() );
+
+$table->cellSet( ++$tableRowIndex, 0, $tagsCellRightMiddle, 'wkHTMLtoPDF' );
+$table->cellSet( $tableRowIndex, 1, $tagsCellRightMiddle, \xan\strSubstitute( shell_exec( 'wkhtmltopdf --version' ), 'wkhtmltopdf', '' ) );
+
+$table->cellSet( ++$tableRowIndex, 0, $tagsCellRightMiddle, '<hr>', 1, 2 );
+
 $table->cellSet( ++$tableRowIndex, 0, $tagsCellRightMiddle, 'Disk Usage' );
 $table->cellSet( $tableRowIndex, 1, $tagsCellRightMiddle, \xan\environ_disk_usage() );
 
@@ -22,8 +33,7 @@ $table->cellSet( $tableRowIndex, 1, $tagsCellRightMiddle, \xan\numBytesToString(
 $table->cellSet( ++$tableRowIndex, 0, $tagsCellRightMiddle, 'Disk Total' );
 $table->cellSet( $tableRowIndex, 1, $tagsCellRightMiddle, \xan\numBytesToString( disk_total_space( "/" ) ) );
 
-$table->cellSet( ++$tableRowIndex, 0, $tagsCellRightMiddle, ' ' );
-$table->cellSet( $tableRowIndex, 1, $tagsCellRightMiddle, ' ' );
+$table->cellSet( ++$tableRowIndex, 0, $tagsCellRightMiddle, '<hr>', 1, 2 );
 
 $table->cellSet( ++$tableRowIndex, 0, $tagsCellRightMiddle, 'RAM Usage' );
 $table->cellSet( $tableRowIndex, 1, $tagsCellRightMiddle, round( \xan\environ_server_memory_used() / \xan\environ_server_memory_total() * 100, 0 ) . '%' );
